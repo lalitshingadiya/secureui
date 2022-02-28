c336809ed41a901baf60984f90537086a2ba6a94
Lalitkumar Shingadiya <lalit.shingadiya@eclinicalworks.com>
2022-02-28 10:11:22 +0530

    initial commite

---
+++ src/test/java/com/ls/secureui/SecureuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ls.secureui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
